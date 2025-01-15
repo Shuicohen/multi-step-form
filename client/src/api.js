4ca32e4b7db89bf4da63d7d807823903f08a3d82
Yehoshua Cohen <100349784+Shuicohen@users.noreply.github.com>
2025-01-15 10:48:19 +0200

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const saveFormData = async (formData) => {
     console.log("Submitting Form Data:", formData); // Debugging
     try {
-        const response = await axios.post("http://localhost:5000/api/form", formData);
+        const response = await axios.post("https://multi-step-form-oho5.onrender.com/api/form", formData);
         console.log("Form saved successfully:", response.data);
         return response.data;
     } catch (error) {
