002fdb0dbec6b31a2b68a133d3b4adb36ebdc0d3
Yehoshua Cohen <100349784+Shuicohen@users.noreply.github.com>
2025-01-16 11:56:14 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import useLocalStorage from "./hooks/useLocalStorage"; // Adjust path if needed
 import FormStep1 from "./components/FormStep1";
 import FormStep2 from "./components/FormStep2";
